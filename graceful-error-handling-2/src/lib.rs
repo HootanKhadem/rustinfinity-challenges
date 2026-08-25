f26b0a4d29910c5af0c81c8bcf1e0b8542d74ce0
hootan <hootankhadem95@gmail.com>
2026-08-25 20:43:50 +0330

    Challenge graceful-error-handling-2 Completed

--- graceful-error-handling-2/src/lib.rs
+++ graceful-error-handling-2/src/lib.rs
@@ -1,10 +1,38 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+
 // 1. Finish the definition
-pub enum ParsePercentageError
+#[derive(Debug, PartialEq)]
+pub enum ParsePercentageError {
+    InvalidInput,
+    OutOfRange,
+}
+
+impl Display for ParsePercentageError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParsePercentageError::InvalidInput => write!(f, "Invalid input"),
+            ParsePercentageError::OutOfRange => write!(f, "Percentage out of range"),
+        }
+    }
+}
 
 // 2. Implement the `Error` trait
+impl Error for ParsePercentageError {}
 
 pub fn parse_percentage(input: &str) -> Result<u8, ParsePercentageError> {
     // 3. Implement this function
+    let parse_result = input.parse::<u8>();
+    if parse_result.is_ok() {
+        let value = parse_result.unwrap();
+        if value <= 100 {
+            Ok(value)
+        } else {
+            Err(ParsePercentageError::OutOfRange)
+        }
+    } else {
+        Err(ParsePercentageError::InvalidInput)
+    }
 }
 
 // Example usage
